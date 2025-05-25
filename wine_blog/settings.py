17ba81ff3e141012bdf42f52d28cbe60299391f4
dinacoding <dinarte.orepolo@gmail.com>
2025-05-25 04:29:02 +0100

    Create a file named env.py and add it on the .gitignore

--- wine_blog/settings.py
+++ wine_blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lxzg8p=cb)+(@+g4khbn1koeo2glr72%2!%udof8pz0nxx#0%5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
