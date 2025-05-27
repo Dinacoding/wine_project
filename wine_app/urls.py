d3a6e1aa0c6499f13a3da2dd50bc221a30703a3c
dinacoding <dinarte.orepolo@gmail.com>
2025-05-27 01:14:58 +0100

    Final confirmation and correction of URL routing

--- wine_app/urls.py
+++ wine_app/urls.py
@@ -3,9 +3,6 @@ from . import views
 
 app_name = 'wine_app'
 
-urlpatterns = [ # <--- THIS MUST BE A LIST!
-    # Add your URL patterns here. At least one is usually needed.
-    path('', views.home_page, name='home'), # Example path
-    # path('wines/', views.wine_list, name='wine_list'), # Another example
-    # path('wines/<slug:wine_slug>/', views.wine_detail, name='wine_detail'),
+urlpatterns = [
+    path('', views.home_page, name='home'),
 ]
