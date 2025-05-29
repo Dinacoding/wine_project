992fe1e4096adc79fa1f1035bd41432a5779b853
dinacoding <dinarte.orepolo@gmail.com>
2025-05-29 12:07:34 +0100

    Create a user model and change the post into wine post

--- wine_app/models.py
+++ wine_app/models.py
@@ -5,13 +5,31 @@ STATUS = ((0, "Draft"), (1, "Published"))
 
 
 # Create your models here.
-class Post(models.Model):
+class User(models.Model):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=150, unique=True)
+    password = models.CharField(max_length=128)
+    
+    def __str__(self):
+        return self.username
+    
+
+class WinePost(models.Model):
+    user = models.ForeignKey(
+    User, 
+    on_delete=models.CASCADE, 
+    related_name="wine_posts"
+    )
+
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
     User, on_delete=models.CASCADE, related_name="blog_posts"
     )
 
+    wine_name = models.CharField(max_length=100)
+    created_on = models.DateTimeField(auto_now_add=True)
+    vintage_year = models.IntegerField() 
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
