91c809382e03a1e0b84c16c1662028fa3d2e4f07
dinacoding <dinarte.orepolo@gmail.com>
2025-06-03 17:54:09 +0100

    Create class winepostadmin and register the model

--- wine_app/admin.py
+++ wine_app/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
-from .models import WinePost 
-# Register your models here.
-admin.site.register(WinePost)
+from .models import WinePost
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(WinePost) 
+class WinePostAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'wine_name', 'vintage_year', 'created_on', 'status')
+    search_fields = ('title', 'wine_name')
+    list_filter = ('status', 'created_on')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
