2179e450a57da8b7be70c385c64865b4ab4139c3
dinacoding <dinarte.orepolo@gmail.com>
2025-06-17 00:40:36 +0100

    Fix views.py indentation and update class-based views

--- wine_app/forms.py
+++ wine_app/forms.py
@@ -10,3 +10,4 @@ class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         fields = ['bio']
+        
--- wine_app/views.py
+++ wine_app/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import WinePost
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin  
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
 
+
+class UserLoginView(LoginView):
+    """
+    View for user login
+    """
+    template_name = 'wine_app/login.html'
+    redirect_authenticated_user = True
+
+class UserLogoutView(LogoutView):
+    """ 
+    View for user logout
+    """
+    template_name = 'wine_app/logout.html'
+    next_page = '/home'  # Redirect to home after logout    
 
 class PostList(generic.ListView):
     """
@@ -10,7 +27,7 @@ class PostList(generic.ListView):
     model = WinePost
     queryset = WinePost.objects.filter(status=1).order_by('-created_on')
     template_name = 'wine_app/index.html'
-    context_object_name = 'wine_posts'  
+    context_object_name = 'wine_posts'
     paginate_by = 6
 
 class PostDetail(generic.DetailView):
@@ -20,10 +37,10 @@ class PostDetail(generic.DetailView):
     model = WinePost
     template_name = 'wine_app/post_detail.html'
     context_object_name = 'post'
-    
+
     def get_queryset(self):
         return WinePost.objects.filter(status=1)
-    
+
 class UserRegisterView(generic.CreateView):
     """
     View for user registration
