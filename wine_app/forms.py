1f7a9f2c8447d079345362ab90d080ec797b75e2
dinacoding <dinarte.orepolo@gmail.com>
2025-06-17 02:17:31 +0100

    Create an user registration form and and the fields to the winePost form and import the models and forms

--- wine_app/forms.py
+++ wine_app/forms.py
@@ -1,5 +1,7 @@
 from django import forms
-from .models import WinePost, UserProfile
+from .models import WinePost, UserProfile   
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User   
 
 class WinePostForm(forms.ModelForm):
     class Meta:
@@ -11,3 +13,17 @@ class UserProfileForm(forms.ModelForm):
         model = UserProfile
         fields = ['bio']
         
+
+class UserRegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
