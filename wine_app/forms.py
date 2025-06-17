4c12e570a1ab985bdd62f5f54f69ed3ff0ba0eb8
dinacoding <dinarte.orepolo@gmail.com>
2025-06-17 05:24:40 +0100

    Remove the aria current

--- wine_app/forms.py
+++ wine_app/forms.py
@@ -27,3 +27,24 @@ class UserRegistrationForm(UserCreationForm):
         if commit:
             user.save()
         return user
+    
+
+class YourFormName(forms.Form):
+    first_name = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter first name'
+        })
+    )
+    last_name = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter last name'
+        })
+    )
+    username = forms.CharField(max_length=150)
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput())
+    confirm_password = forms.CharField(widget=forms.PasswordInput())
