f6b6e20e1f853e7f205071f4cb488e6b90106b39
dinacoding <dinarte.orepolo@gmail.com>
2025-06-22 22:29:25 +0100

    Change the Postdetailviews into PostDetail in the urlpatterns

--- wine_app/urls.py
+++ wine_app/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('blog/', views.BlogListView.as_view(), name='blog'),
-    path('post/<slug:slug>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('post/<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('register/', views.UserRegisterView.as_view(), name='register'),
     path('login/', views.UserLoginView.as_view(), name='login'),
     path('logout/', views.UserLogoutView.as_view(), name='logout'),
