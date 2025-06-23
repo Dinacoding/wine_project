51b7ed48e8c13ca55525d2640e31488853f5188d
dinacoding <dinarte.orepolo@gmail.com>
2025-06-24 00:57:11 +0100

    Remove the profile path in the urlpatterns

--- wine_blog/urls.py
+++ wine_blog/urls.py
@@ -5,9 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
     path('summernote/', include('django_summernote.urls')),
-
     path('', include('wine_app.urls')),  # This should be LAST to avoid conflicts
 ]
 
