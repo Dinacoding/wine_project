41fee877eb338357f545a84c5a184c148e57888e
dinacoding <dinarte.orepolo@gmail.com>
2025-06-24 03:09:16 +0100

    Create a home url

--- wine_app/urls.py
+++ wine_app/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='home'),
+    path('home', views.HomeView.as_view(), name='home'),
     path('blog/', views.BlogListView.as_view(), name='blog'),
     path('post/<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('register/', views.UserRegisterView.as_view(), name='register'),
