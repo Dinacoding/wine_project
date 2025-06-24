5bf9a381cfac9bc5a38a5c3c43fe00710bf3eada
dinacoding <dinarte.orepolo@gmail.com>
2025-06-24 17:23:55 +0100

    Upadate the views and the blog.html

--- wine_app/urls.py
+++ wine_app/urls.py
@@ -6,8 +6,10 @@ urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('blog/', views.BlogListView.as_view(), name='blog'),
     path('post/<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('post/<slug:slug>/delete/', views.delete_post, name='delete_post'),
     path('register/', views.UserRegisterView.as_view(), name='register'),
     path('login/', views.UserLoginView.as_view(), name='login'),
     path('logout/', views.UserLogoutView.as_view(), name='logout'),
+
 ]
 
