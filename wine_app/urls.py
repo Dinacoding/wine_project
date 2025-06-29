4a8d91d250a7c9a04f13fd4fb8f97b824d6c692b
dinacoding <dinarte.orepolo@gmail.com>
2025-06-29 14:59:13 +0100

    Update and organise the wine_app/urls.py

--- wine_app/urls.py
+++ wine_app/urls.py
@@ -1,16 +1,19 @@
+# wine_app/urls.py
+
 from django.urls import path
 from . import views
 
 
 urlpatterns = [
+    # General Pages / Auth (Class-Based Views)
     path('', views.HomeView.as_view(), name='home'),
-    path('blog/', views.BlogListView.as_view(), name='blog'),
-    path('post/<slug:slug>/', views.PostDetail, name='post_detail'),
-    path('post/create', views.PostCreateView.as_view(), name='create_post'),
-    path('post/<slug:slug>/delete/', views.delete_post, name='delete_post'),
+    path('blog/', views.BlogListView.as_view(), name='blog'), 
     path('register/', views.UserRegisterView.as_view(), name='register'),
     path('login/', views.UserLoginView.as_view(), name='login'),
     path('logout/', views.UserLogoutView.as_view(), name='logout'),
 
+    # Post Management (Function-Based Views)
+    path('post/create/', views.create_post, name='create_post'), 
+    path('post/<slug:slug>/', views.PostDetail, name='post_detail'), 
+    path('post/<slug:slug>/delete/', views.delete_post, name='delete_post')
 ]
-
